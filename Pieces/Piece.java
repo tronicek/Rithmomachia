84fd507d94893c8fa6021903396ec5c6b7a4de39
Outsider20 <Spoojr848@oneonta.edu>
2024-09-16 13:23:54 -0400

    Update Piece.java

--- Pieces/Piece.java
+++ Pieces/Piece.java
@@ -4,42 +4,13 @@ public abstract class Piece {
     private ArrayList<Integer> listValue;
     private int movement;
     private String color;
+    private String type
 
 // Constructor For the Circle, Triangle and Square pieces
-    public Piece(String shape, int value, int movement, String color) {
+    public Piece(String shape, int value, int movement, String color, String type) {
         this.shape = shape;
         this.value = value;
         this.movement = movement;
         this.color = color;
+        this.type = type
     }
-
-// Constructor For the Pyramid pieces
-    public Piece(String shape, ArrayList<Integer> listValue, int movement, String color) {
-        this.shape = shape;
-        this.listValue = new ArrayList<>();
-        this.movement = movement;
-        this.color = color;
-    }
-
-    public String getShape() {
-        return shape;
-    }
-
-    public int getValue() {
-        return listValue;
-    }
-    
-    public int getValue() {
-        return value;
-    }
-
-    public int getMovement() {
-        return movement;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public abstract boolean isValidMove(int startX, int startY, int endX, int endY, Board board);
-}
