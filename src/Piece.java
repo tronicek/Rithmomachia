288ff4eb9bfeed94607dac9a21b63703da516978
PL0PPER <jakepincus0@gmail.com>
2025-01-30 15:25:25 -0500

    Update Piece.java Initialized Deceit Capture

--- src/Piece.java
+++ src/Piece.java
@@ -48,7 +48,27 @@ public abstract class Piece {
 		}
 		return posECaps;
 	}
+	/*public Set<Pos> deceitCapture(Board board) {
+		Set<Pos> posDCaps = new HashSet<>();
+  		Set<Piece> neighbors = board.findClosestNeighbors(this.x, this.y);
+    		if (!neighbors.isEmpty()){
+      			for(Piece piece1 : neighbors) { // Here making sure two diff pieces
+	 			for(Piece piece2 : neighbors){
+     					if (piece1 != piece2){ //check if pieces are on opposite sides of piece
+	  					boolean isOpposite = (piece1.x == piece2.x || piece1.y == piece2.y);
 	
+						if (isOpposite) { //check if sum of the values = current piece value
+      							if (piece1.value + piece2.value == this.value) { //add both pieces' positions as working deceit captures
+	     							posDCaps.add(new Pos(piece1.x, piece1.y));
+	     							posDCaps.add(new Pos(piece2.x, piece2.y));
+	     						}
+	    					}
+	  				}
+       				}
+	   		}
+      		}
+	}
+	*/
 	public Color getColor() {
 		return this.color;
 	}
