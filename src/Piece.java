98beeaeac6a8117ad822de8caf0b4aa52381d333
Nammari Drayton <Draynt796@oneonta.edu>
2025-02-13 17:21:33 -0500

    fix

--- src/Piece.java
+++ src/Piece.java
@@ -218,7 +218,7 @@ public abstract class Piece {
 		return "   ";
 	}
 	public String toString() {
-		return String.format("%s%s%d", color, getShape(), value);
+		return String.format("%s%s%d", color, shape, value);
 	}
 
 }
