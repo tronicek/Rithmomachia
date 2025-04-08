e663468b7046de78ccceb682cf12d812eb982f21
Nammari Drayton <Draynt796@oneonta.edu>
2025-04-08 12:31:09 -0400

    Added More Siege Test Cases

--- src/test/rithmomachia/CaptureBySiegeTest.java
+++ src/test/rithmomachia/CaptureBySiegeTest.java
@@ -132,4 +132,59 @@ public class CaptureBySiegeTest {
 
     }
 
+    @Test
+    public void test8() {
+        String[] s = {
+                "WC1 --- P,W,C50,S20,T70",
+                "--- P,B,C50,S10,T70 ---",
+                "WC1 --- WC1"
+        };
+        Board b = new Board(3, 3, s);
+        b.printBoard();
+        Piece p = b.getPiece(0, 2);
+        Set<Piece> pp = p.captureBySiege(b);
+        assertEquals(1,pp.size());
+
+
+
+    }
+
+    @Test
+    public void test9() {
+        String[] s = {
+                "--- --- WS17 --- ---",
+                "--- WS11 BT4 WS9 ---",
+                "WC15 BC2 WS5 BS6 WC3",
+                "--- WT13 BT8 WT7 ---",
+                "--- --- WS19 --- ---",
+        };
+        Board b = new Board(5, 5, s);
+        b.printBoard();
+        Piece p = b.getPiece(2, 2);
+        Set<Piece> pp = p.captureBySiege(b);
+        assertEquals(4,pp.size());
+
+
+
+    }
+
+    @Test
+    public void test10() {
+        String[] s = {
+                "WT13 --- WC40 --- P,W,C50,S20,T70",
+                "--- BS11 WT4 BS9 ---",
+                "WC5 WC2 WS5 WS6 WT70",
+                "--- BT13 WT8 BT7 ---",
+                "WC30 --- WT60 --- WS20",
+        };
+        Board b = new Board(5, 5, s);
+        b.printBoard();
+        Piece p = b.getPiece(2, 2);
+        Set<Piece> pp = p.captureBySiege(b);
+        assertEquals(4,pp.size());
+
+
+
+    }
+
 }
